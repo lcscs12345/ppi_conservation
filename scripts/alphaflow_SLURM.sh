7b5230d686edc208df36d00ad80044a2014b094c
Chun Shen Lim <chunshen.lim@otago.ac.nz>
2024-11-12 08:59:15 +1300

    add rmsf analysis

--- scripts/alphaflow_SLURM.sh
+++ scripts/alphaflow_SLURM.sh
@@ -4,8 +4,8 @@
 #SBATCH --time=24:00:00
 
 #log files:
-#SBATCH -e /home/limch05p/doc/alphaflow/logs/alphaflow_%A_%a_err.txt
-#SBATCH -o /home/limch05p/doc/alphaflow/logs/alphaflow_%A_%a_out.txt
+#SBATCH -e /home/limch05p/lim_group/ppi_conservation/logs/alphaflow_%A_%a_err.txt
+#SBATCH -o /home/limch05p/lim_group/ppi_conservation/logs/alphaflow_%A_%a_out.txt
 
 #SBATCH -p aoraki_gpu_A100
 #SBATCH --gres=gpu:1
@@ -13,23 +13,25 @@
 #SBATCH --mem-per-cpu=4000
 
 
-WORK_DIR=/home/limch05p/doc/alphaflow/
-GPUs=`apptainer exec --nv /home/limch05p/src/alphaflow.sif python -c "import torch; print(torch.cuda.device_count())"`
+WORK_DIR=/home/limch05p/lim_group/ppi_conservation/data
+SRC_DIR=/home/limch05p/lim_group/ppi_conservation/src
+GPUs=`apptainer exec --nv ${SRC_DIR}/alphaflow.sif python -c "import torch; print(torch.cuda.device_count())"`
 
 if [ $GPUs != 0 ]
   then
     echo "${GPUs} GPUs found..."
-
+    
+    mkdir -p ${WORK_DIR}/tmp/
     awk "NR==($SLURM_ARRAY_TASK_ID)+1" ${WORK_DIR}/selected.csv \
     | sed '1i name,seqres' > ${WORK_DIR}/tmp/${SLURM_ARRAY_TASK_ID}.csv
 
     apptainer exec \
-    --nv /home/limch05p/src/alphaflow_20240515.sif \
-    python /home/limch05p/src/alphaflow/predict.py \
+    --nv ${SRC_DIR}/alphaflow_20240515.sif \
+    python ${SRC_DIR}/alphaflow/predict.py \
     --mode esmfold \
     --input_csv ${WORK_DIR}/tmp/${SLURM_ARRAY_TASK_ID}.csv \
-    --weights /home/limch05p/src/alphaflow/weights/esmflow_md_base_202402.pt \
-    --outpdb ${WORK_DIR}/output/ \
+    --weights ${SRC_DIR}/alphaflow/weights/esmflow_md_base_202402.pt \
+    --outpdb ${WORK_DIR} \
     --samples 250
   else
     echo " No GPUs found!"
--- scripts/usalign_SLURM.sh
+++ scripts/usalign_SLURM.sh
@@ -4,8 +4,8 @@
 #SBATCH --time=08:00:00
 
 #log files:
-#SBATCH -e /home/limch05p/doc/alphaflow/logs/usalign_%A_err.txt
-#SBATCH -o /home/limch05p/doc/alphaflow/logs/usalign_%A_out.txt
+#SBATCH -e /home/limch05p/lim_group/ppi_conservation/logs/usalign_%A_err.txt
+#SBATCH -o /home/limch05p/lim_group/ppi_conservation/logs/usalign_%A_out.txt
 
 #Adjust this depending on the node
 #SBATCH --cpus-per-task=1
@@ -14,8 +14,9 @@
 source /home/limch05p/anaconda3/etc/profile.d/conda.sh
 conda activate utils
 
-WORK_DIR=/home/limch05p/doc/alphaflow/output
-FILE_PATH=$( ls -1 $WORK_DIR/*.pdb | grep relaxed )
+WORK_DIR=/home/limch05p/lim_group/ppi_conservation/data
+SCRIPTS_DIR=/home/limch05p/lim_group/ppi_conservation/src
+FILE_PATH=$( ls -1 $WORK_DIR/*.pdb )
 TASK=$( echo "$FILE_PATH" | awk "NR==$SLURM_ARRAY_TASK_ID" )
 
-python ~/scripts/usalign_multimodels.py -i $TASK -s 250 -r 6ahy.pdb -c A,B
+python ${SCRIPTS_DIR}/usalign_multimodels.py -i $TASK -s 250 -r $WORK_DIR/6ahy.pdb -c A,B
